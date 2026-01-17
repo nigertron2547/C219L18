46ee78d70e0034a77247750a3a35ea7db3eab697
24023374_LoiYi <24023374@myrp.edu.sg>
2026-01-17 15:33:05 +0800

    Modify Confirm page

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -6,8 +6,8 @@ return (
     <div className="container">
         {state && (
         <>
-        <p>Congrats<strong>{state.name}</strong>, you're now registered for the {state.course}.</p>
-        <p>We've sent more details to <strong>{state.email}</strong></p>
+        <p>Congrats <strong>{state.name}</strong>, you're now registered for the <strong>{state.course}</strong>.</p>
+        <p>We've sent more details to <strong>{state.email}.</strong></p>
         </>
     )}
 
